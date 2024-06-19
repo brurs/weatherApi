fe2570529203353ed99e60c007198ecae5a7d347
DESKTOP-KRB0HUF\brstu <brstupp@hotmail.com>
2024-06-19 18:13:40 -0300

    criação de testes unitarios

---
+++ TestProject/WeatherTest.cs
@@ -0,0 +1,47 @@
+﻿using DataStorage.DataAccess;
+using DataStorage.Model;
+using Domain.Command;
+using Domain.Handler;
+using Domain.Query;
+using Moq;
+
+namespace WeatherMap.Test
+{
+    public class WeatherTest
+    {
+        [Fact]
+        public async Task GetWeatherList_returnsFiltered() 
+        {
+            var repositoryMock = new Mock<IDataAccess>();
+            repositoryMock.Setup(r => r.Weather).Returns(Weathers);
+
+            var handler = new GetWeatherListHandler(repositoryMock.Object);
+
+            var result = await handler.Handle(new GetWeatherListQueryRequest { Cidade = "Florianopolis, Curitiba", Fim = DateTime.Now.AddDays(10), Inicio = DateTime.Now.AddDays(-10) }, new CancellationToken());
+
+            Assert.True(result.Count() == 2);
+        }
+
+        [Fact]
+        public async Task CreateWeather_AddsItems()
+        {
+            var repo = new WeatherDataAccess();
+            var handler = new CreateWeatherHandler(repo);
+
+            var result = await handler.Handle(new CreateWeatherCommandRequest(), new CancellationToken());
+
+            Assert.True(repo.Weather.Count() == 3);
+        }
+
+        private List<Weather> Weathers {
+            get
+            {
+                return new List<Weather> {
+                    new Weather { Cidade = "Florianopolis", Data = DateTime.Now, Temperatura = 20.4d },
+                    new Weather { Cidade = "Porto Alegre", Data = DateTime.Now, Temperatura = 12.3d },
+                    new Weather { Cidade = "Curitiba", Data = DateTime.Now, Temperatura = 6.2d }
+                };
+            }
+        }
+    }
+}
---
+++ WeatherMap/PerguntasTecnicas.md
@@ -0,0 +1,14 @@
+1 - Usei cerca de 10h para o desenvolvimento.
+	Poderia ter feito mais rapidamente porem utilizei esse projeto para estudar alguns tópicos.
+	Ja havia desenvolvido em sistemas que utilizava CQRS e Mediatr, porem nunca criado e configurado um projeto nesse modelo do zero.
+
+2 - Nenhum framework, porem utilizei algumas bibliotecas para auxiliar. 
+	Mediatr para facilitar a gestão de injeções entre projetos no modelo de projeto que escolhi.
+	Newtonsoft.Json para facilitar a interpretação de retorno da API externa.
+	Moq e xunit para o projeto de testes.
+
+3 - 
+{
+	
+}
+	
